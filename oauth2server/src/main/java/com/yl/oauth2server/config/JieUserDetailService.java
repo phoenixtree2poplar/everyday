80044cfa04916726b10f73b71b370d143cb592bd
yangjie <18202975766@163.com>
2019-12-03 20:53:51 +0800

    扩展用户信息

--- oauth2server/src/main/java/com/yl/oauth2server/config/JieUserDetailService.java
+++ oauth2server/src/main/java/com/yl/oauth2server/config/JieUserDetailService.java
@@ -1,15 +1,19 @@
 package com.yl.oauth2server.config;
 
+import com.alibaba.fastjson.JSON;
 import com.yl.common.dao.UserMapper;
 import com.yl.common.dao.UserRoleMapper;
 import com.yl.common.pojo.Role;
 import com.yl.common.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -20,6 +24,7 @@ import java.util.List;
 @Component
 public class JieUserDetailService implements UserDetailsService {
 
+    private List<Role> roles;
     @Autowired
     private UserMapper userMapper;
     @Autowired
@@ -34,7 +39,19 @@ public class JieUserDetailService implements UserDetailsService {
         }
         String userId = user.getId();
         List<Role> roles = userRoleMapper.getRolesByUserId(userId);
-        return new JieUserDetails(user, roles);
+        String userJson = JSON.toJSONString(user);
+        return new org.springframework.security.core.userdetails.User(userJson, user.getPassword(), getAuthorities(roles));
     }
 
+    public Collection<? extends GrantedAuthority> getAuthorities(List<Role> roles) {
+        if (roles == null || roles.size() < 1) {
+            return AuthorityUtils.commaSeparatedStringToAuthorityList("");
+        }
+        StringBuilder commaBuilder = new StringBuilder();
+        for (Role role : roles) {
+            commaBuilder.append("ROLE_" + role.getName()).append(",");
+        }
+        String authorities = commaBuilder.substring(0, commaBuilder.length() - 1);
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+    }
 }
--- userservice/src/main/java/com/yl/userservice/controller/UserController.java
+++ userservice/src/main/java/com/yl/userservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.yl.userservice.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.yl.common.pojo.User;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +22,10 @@ public class UserController {
     //标记拥有ROLE_ADMIN权限可以访问资源
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     public String get() {
-        SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = JSONObject.parseObject(principal.toString(), User.class);
+        System.out.println(user.getUsername());
+        System.out.println(user.getId());
         return "访问资源1";
     }
 
